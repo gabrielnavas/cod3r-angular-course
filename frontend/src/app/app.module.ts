014e64e81297d2543b66b7c0b8836b0974b00153
navas <gabrielnavas@email.com>
2023-09-26 20:20:11 -0300

    add route router outlet on content sidenav content

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,14 +12,16 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatListModule } from '@angular/material/list'
 
 import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component'
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
