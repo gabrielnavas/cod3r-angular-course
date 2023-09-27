dddd7bbf621d7d0747475ebb06788b2e4545f14c
navas <gabrielnavas@email.com>
2023-09-26 23:14:35 -0300

    example structural directive

--- frontend/src/app/directives/for.directive.ts
+++ frontend/src/app/directives/for.directive.ts
@@ -1,18 +1,25 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[myFor]'
 })
 export class ForDirective implements OnInit {
 
-  @Input('myForEm') numbers: number[]
-  @Input('myForUsando') texto: string
+  /**
+   * usando a chamada
+   * <li *myFor="let n em [1,2,3] usando 'teste'">{{ n }}</li>
+   */
+  @Input('myForEm') numbers: number[] //recebe o valor após o em, [1,2,3]
+  @Input('myForUsando') texto: string // recebe o valor após o usando, 'teste'
 
-  constructor() { }
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) { }
 
   ngOnInit(): void {
-    console.log(this.numbers)
-    console.log(this.texto)
+    for(let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number }) // adiciona o valor de n 
+    }
   }
-
 }
