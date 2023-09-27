ff260f2a4be9c74195d75d62a3eb25bcbb7cf8b5
navas <gabrielnavas@email.com>
2023-09-27 18:21:21 -0300

    added material form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,10 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from '@angular/material/button'
 import { MatSnackBarModule } from '@angular/material/snack-bar'
 
+import { FormsModule } from "@angular/forms"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+
 
 @NgModule({
   declarations: [
@@ -48,7 +52,10 @@ import { MatSnackBarModule } from '@angular/material/snack-bar'
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
